d52a86c6d0cf7cf78b4100cc5295aed19da41c31
trimonuter <mrqaist@gmail.com>
2022-11-13 14:00:44 +0700

    [4] pigLatin.js

--- solveProblem.js
+++ solveProblem.js
@@ -1,13 +1,13 @@
-// url: https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/javascript
-// program: 3_whoLikes.js
+// url: https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+// program: 4_pigLatin.js
 
-function likes(names) {
-  whoLikes = 
-  (names.length == 0) ? 'no one likes'
-  : (names.length == 1) ? `${names[0]} likes`
-  : (names.length == 2) ? `${names[0]} and ${names[1]} like`
-  : (names.length == 3) ? `${names[0]}, ${names[1]} and ${names[2]} like`
-  : `${names[0]}, ${names[1]} and ${names.length - 2} others like`
-
-  return `${whoLikes} this`
+function pigIt(text){
+  let textArray = text.split(" ")
+  for (i = 0; i < textArray.length; i++){
+    if ('?!'.includes(textArray[i]) == false){
+      word = textArray[i]
+      textArray[i] = word.slice(1) + word[0] + 'ay'
+    }
+  }
+  return textArray.join(" ")
 }
