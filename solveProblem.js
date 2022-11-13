9921b8d159451d175da4ff8fcb823598bbf3eab3
trimonuter <mrqaist@gmail.com>
2022-11-13 13:21:15 +0700

    [2] sortOddNumbers.js

--- solveProblem.js
+++ solveProblem.js
@@ -1,9 +1,18 @@
-// url: https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/javascript
-// program: 1_basicOperations.js
+// url: https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript
+// program: 2_sortOddNumbers.js
 
-function basicOp(op, a, b){
-  return (op == '+') ? a + b 
-  : (op == '-') ? a - b 
-  : (op == '*') ? a * b
-  : a / b
+function sortArray(arr){
+  let temp = 0
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] % 2 != 0){
+      for(let j = i + 1; j < arr.length; j++){
+        if ((arr[j] % 2 != 0) && (arr[j] < arr[i])){
+          temp = arr[i]
+          arr[i] = arr[j]
+          arr[j] = temp
+        }
+      }
+    }
+  }
+  return arr
 }
