fd70c0750267c385eaae19b94ae4eb9da48c27b1
trimonuter <mrqaist@gmail.com>
2022-12-15 19:13:31 +0700

    [5] supermarketQueue.js

--- solveProblem.js
+++ solveProblem.js
@@ -1,13 +1,41 @@
-// url: https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
-// program: 4_pigLatin.js
+// url: https://www.codewars.com/kata/57b06f90e298a7b53d000a86/train/javascript
+// program: 5_supermarketQueue.js
 
-function pigIt(text){
-  let textArray = text.split(" ")
-  for (i = 0; i < textArray.length; i++){
-    if ('?!'.includes(textArray[i]) == false){
-      word = textArray[i]
-      textArray[i] = word.slice(1) + word[0] + 'ay'
-    }
+function queueTime(customers, n){
+  console.log(customers, n);
+  if (customers.length <= n){
+    return (customers.length === 0) ? 0 : Math.max(...customers);
   }
-  return textArray.join(" ")
+  if (n === 1){
+    let time = 0;
+    for (let i = 0; i < customers.length; i++){
+      time += customers[i];
+    }
+    return time;
+  }
+  let i = 0;
+  let time = 0;
+  while (i < customers.length){
+    i = 0;
+    let servedCustomers = [];
+    while (servedCustomers.length < n){
+      if (customers[i] > 0){
+        servedCustomers.push(customers[i]);
+      }
+      i++;
+    }
+    for (let j = 0; j < i; j++){
+      customers[j] -= Math.min(...servedCustomers);
+    }
+    if (i < customers.length){
+      time += Math.min(...servedCustomers);
+    }
+    else{
+      time += Math.max(...servedCustomers);
+    }
+    console.log(time)
+  }
+  return time;
 }
+
+// Success, but code timeout
